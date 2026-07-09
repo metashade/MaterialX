154e59f9209b136bfa978a2673a0563e45dba95e
Pavlo Penenko <pavlo.penenko@autodesk.com>
2026-07-09 10:04:12 -0400

    Fix UsdPreviewSurface black render: use inout for closure output params
    Update metashade submodule and regenerate GLSL references to use
    `inout` instead of `out` for BSDF/EDF/VDF output parameters.
    
    Closure nodes have early-return paths (e.g. weight=0) that rely on the
    caller's initial identity BSDF being preserved.  The `out` qualifier
    discarded that value, producing undefined throughput in layer operations
    when clearcoat=0 (all UsdPreviewSurface materials).
    
    Fixes #26.
    
    Signed-off-by: Pavlo Penenko <pavlo.penenko@autodesk.com>

--- contrib/tests/metashade_ref/libraries/broken_schlick/mx_generalized_schlick_bsdf_broken_genglsl_impl.glsl
+++ contrib/tests/metashade_ref/libraries/broken_schlick/mx_generalized_schlick_bsdf_broken_genglsl_impl.glsl
@@ -1,5 +1,5 @@
 #include "mx_generalized_schlick_bsdf.glsl"
-void mx_generalized_schlick_bsdf_broken(ClosureData closureData, float weight, vec3 color0, vec3 color82, vec3 color90, float exponent, vec2 roughness, bool retroreflective, float thinfilm_thickness, float thinfilm_ior, vec3 normal, vec3 tangent, int distribution, int scatter_mode, out BSDF out_)
+void mx_generalized_schlick_bsdf_broken(ClosureData closureData, float weight, vec3 color0, vec3 color82, vec3 color90, float exponent, vec2 roughness, bool retroreflective, float thinfilm_thickness, float thinfilm_ior, vec3 normal, vec3 tangent, int distribution, int scatter_mode, inout BSDF out_)
 {
 	out_.response = vec3(1.0, 0.0, 0.5);
 	out_.throughput = vec3(0.0, 0.0, 0.0);
