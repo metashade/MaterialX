4e1244bc5bd057b74381563100689c5e994ff516
Pavlo Penenko <pavlo.penenko@autodesk.com>
2026-08-18 12:07:27 -0400

    Add subsurface scattering to Metashade Standard Surface
    - Update Metashade submodule to main with SSS implementation
    - Regenerate library and render shader baselines
    - Add standard_surface_jade.mtlx to test list (exercises SSS path)
    
    Signed-off-by: Pavlo Penenko <pavlo.penenko@autodesk.com>

--- contrib/tests/test_render_metashade.py
+++ contrib/tests/test_render_metashade.py
@@ -241,6 +241,7 @@ _STANDARD_SURFACE_TEST_PATHS = (
     "Examples/StandardSurface/standard_surface_metal_brushed.mtlx",
     "Examples/StandardSurface/standard_surface_velvet.mtlx",
     "Examples/StandardSurface/standard_surface_carpaint.mtlx",
+    "Examples/StandardSurface/standard_surface_jade.mtlx",
 )
 
 
