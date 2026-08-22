97264e6dc3042d464cb93e7fa57bac9e40c85191
Pavlo Penenko <1525958+ppenenko@users.noreply.github.com>
2026-08-22 12:56:32 -0400

    Implement PyMaterialXRenderSlang Python bindings (#57)

---
+++ source/PyMaterialX/PyMaterialXRenderSlang/PyModule.cpp
@@ -0,0 +1,24 @@
+//
+// Copyright Contributors to the MaterialX Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <PyMaterialX/PyMaterialX.h>
+
+namespace py = pybind11;
+
+void bindPySlangRenderer(py::module& mod);
+void bindPySlangTextureHandler(py::module& mod);
+void bindPyTextureBaker(py::module& mod);
+
+PYBIND11_MODULE(PyMaterialXRenderSlang, mod)
+{
+    mod.doc() = "Rendering support for the Slang shading language.";
+
+    // PyMaterialXRenderSlang depends on types defined in PyMaterialXRender
+    PYMATERIALX_IMPORT_MODULE(PyMaterialXRender);
+
+    bindPySlangRenderer(mod);
+    bindPySlangTextureHandler(mod);
+    bindPyTextureBaker(mod);
+}
---
+++ source/PyMaterialX/PyMaterialXRenderSlang/PySlangRenderer.cpp
@@ -0,0 +1,27 @@
+//
+// Copyright Contributors to the MaterialX Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <PyMaterialX/PyMaterialX.h>
+
+#include <MaterialXRenderSlang/SlangRenderer.h>
+
+namespace py = pybind11;
+namespace mx = MaterialX;
+
+void bindPySlangRenderer(py::module& mod)
+{
+    py::class_<mx::SlangRenderer, mx::ShaderRenderer, mx::SlangRendererPtr>(mod, "SlangRenderer")
+        .def_static("create", &mx::SlangRenderer::create,
+            py::arg("width") = 512, py::arg("height") = 512, py::arg("baseType") = mx::Image::BaseType::UINT8)
+        .def("initialize", &mx::SlangRenderer::initialize, py::arg("renderContextHandle") = nullptr)
+        .def("createImageHandler", &mx::SlangRenderer::createImageHandler)
+        .def("createProgram", static_cast<void (mx::SlangRenderer::*)(const mx::ShaderPtr)>(&mx::SlangRenderer::createProgram))
+        .def("createProgram", static_cast<void (mx::SlangRenderer::*)(const mx::SlangRenderer::StageMap&)>(&mx::SlangRenderer::createProgram))
+        .def("validateInputs", &mx::SlangRenderer::validateInputs)
+        .def("render", &mx::SlangRenderer::render)
+        .def("renderTextureSpace", &mx::SlangRenderer::renderTextureSpace)
+        .def("captureImage", &mx::SlangRenderer::captureImage)
+        .def("getProgram", &mx::SlangRenderer::getProgram);
+}
